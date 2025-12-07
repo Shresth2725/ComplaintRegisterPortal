bac9e6b7a01df4abd647fd31d9e5703b3b50037d
Shresth <2725Shresth@gmail.com>
2025-12-08 02:21:25 +0530

    baseUrl upate

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,34 +1,28 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
-// Add request interceptor for debugging
-API.interceptors.request.use(
-  (config) => {
-    console.log('Request:', config.method.toUpperCase(), config.baseURL + config.url);
-    return config;
-  },
-  (error) => {
-    console.error('Request error:', error);
-    return Promise.reject(error);
-  }
-);
+// logging...
+API.interceptors.request.use((config) => {
+  console.log(
+    "Request:",
+    config.method.toUpperCase(),
+    config.baseURL + config.url
+  );
+  return config;
+});
 
-// Add response interceptor for debugging
 API.interceptors.response.use(
-  (response) => {
-    console.log('Response:', response.status, response.data);
-    return response;
-  },
-  (error) => {
-    console.error('Response error:', error.response?.status, error.message);
-    return Promise.reject(error);
+  (res) => res,
+  (err) => {
+    console.error("Response error:", err.response?.status, err.message);
+    return Promise.reject(err);
   }
 );
 
